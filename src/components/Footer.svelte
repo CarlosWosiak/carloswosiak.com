d2ef06b5a5b754f3f2ac62a1a13ab1ca9b622a39
Carlos Wosiak <carlosw403@hotmail.com>
2023-09-28 22:12:26 -0300

    feat(footer): add max width on credits

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,8 +6,10 @@
     <div
         class="col-sm-10 col-md-5 col-lg-4 col-sm-offset-1 col-lg-offset-3 col-md-offset-2"
     >
-        Built with Svelte, mini.css and ❤️, deployed with Vercel. All text is
-        set in the Inter typeface.
+        <div class="credit">
+            Built with Svelte, mini.css and ❤️, deployed with Vercel. All text
+            is set in the Inter typeface.
+        </div>
     </div>
 
     <div class="col-sm-10 col-md-3 col-lg-2 col-sm-offset-1">
@@ -18,5 +20,8 @@
 <style>
     .footer {
         margin-bottom: 4rem;
+    }
+    .credit {
+        max-width: 20rem;
     }
 </style>
