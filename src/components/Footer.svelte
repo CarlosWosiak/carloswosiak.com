bb37307556178dffb1f9f12d9c5761a9add30730
Carlos Wosiak <carlosw403@hotmail.com>
2023-09-28 22:08:47 -0300

    feat(app): add social media icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,15 @@
+<script>
+    import Social from "./Social.svelte";
+</script>
+
 <div class="row footer">
     <div
-        class="col-sm-10 col-md-3 col-lg-2 col-sm-offset-1 col-lg-offset-7 col-md-offset-8"
+        class="col-sm-10 col-md-5 col-lg-4 col-sm-offset-1 col-lg-offset-3 col-md-offset-2"
     >
+        <Social />
+    </div>
+
+    <div class="col-sm-10 col-md-3 col-lg-2 col-sm-offset-1">
         Built with Svelte, mini.css and ❤️, deployed with Vercel. All text is
         set in the Inter typeface.
     </div>
