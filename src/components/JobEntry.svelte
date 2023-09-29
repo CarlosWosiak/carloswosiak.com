8fcbef670eca166c002353f385f770a08542f091
Carlos Wosiak <carlosw403@hotmail.com>
2023-09-29 09:53:11 -0300

    refactor(animations): remove console log

--- src/components/JobEntry.svelte
+++ src/components/JobEntry.svelte
@@ -11,7 +11,6 @@
     onMount(() => {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
-                console.log(entry);
                 if (entry.isIntersecting) {
                     entry.target.classList.add("show");
                 }
