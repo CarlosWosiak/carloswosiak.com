4cdf0d4dc0c99936133ac5abce0f96e460505611
Carlos Wosiak <carlosw403@hotmail.com>
2024-10-14 14:12:26 -0300

    Add iterative job

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,51 +1,65 @@
-export const jobs = [{
-    startDate: "2021",
+export const jobs = [
+  {
+    startDate: "2024",
     endDate: "Present",
+    jobTitle: "Senior Software Engineer",
+    company: "Iterative;",
+    description: [
+      "Consulting for ConectCar, a company in the vehicle toll-tag industry. My work focuses developing robust, scalable solutions using .NET Core, with a strong emphasis on chatbot integration and advanced cloud infrastructure.",
+    ],
+    stack: ["C#", "Azure", "BotPress", "Angular", ".NET", "Docker"],
+  },
+  {
+    startDate: "2021",
+    endDate: "2024",
     jobTitle: "Software Engineer",
     company: "Itaú Unibanco",
     description: [
-        'Develop highly scalable AWS Lambda functions using Python and .NET, efficiently processing millions of trading operations daily, resulting in a significant increase of productivity and cost saving for the organization,',
-        'Play a key role in the successful migration of  the company’s infrastructure to the AWS cloud.',
-        'Build and ship systems using mostly AWS services, such as DynamoDB, RDS, Lambdas, Step Functions, CloudWatch, S3, SNS, SQS and EC2 instances.',
-        'Recognized as a top talent within the organization, receiving 100% monthly salary bonus as a recognition for the results presented.',
-        'Deliver high-quality, robust production code for a diverse array of projects, including trading sessions with Bloomberg, CitiBank, JPMorgan and Credit Suisse.',
-        'Implement data analytics using AWS tools (Kinesis, Glue, Firehose) to extract insights, enhancing decision-making within the corporate treasury.',
-        'Automated 80% of trade processing operations, handling millions of transactions, resulting in increased efficiency and significant cost savings.'
+      "Develop highly scalable AWS Lambda functions using Python and .NET, efficiently processing millions of trading operations daily, resulting in a significant increase of productivity and cost saving for the organization,",
+      "Play a key role in the successful migration of  the company’s infrastructure to the AWS cloud.",
+      "Build and ship systems using mostly AWS services, such as DynamoDB, RDS, Lambdas, Step Functions, CloudWatch, S3, SNS, SQS and EC2 instances.",
+      "Recognized as a top talent within the organization, receiving 100% monthly salary bonus as a recognition for the results presented.",
+      "Deliver high-quality, robust production code for a diverse array of projects, including trading sessions with Bloomberg, CitiBank, JPMorgan and Credit Suisse.",
+      "Implement data analytics using AWS tools (Kinesis, Glue, Firehose) to extract insights, enhancing decision-making within the corporate treasury.",
+      "Automated 80% of trade processing operations, handling millions of transactions, resulting in increased efficiency and significant cost savings.",
     ],
-    stack: ['C#', 'AWS', 'Python', 'Angular', '.NET', 'Docker', 'Kafka']
-}, {
+    stack: ["C#", "AWS", "Python", "Angular", ".NET", "Docker", "Kafka"],
+  },
+  {
     startDate: "2020",
     endDate: "2021",
     jobTitle: "Software Engineer",
     company: "Kolina Labs",
     description: [
-    'Developed dockerized applications in NodeJS, Laravel, Vue, Python and Svelte.', 
-    'Maintained a reliable platform with millions of dollars transacted monthly.',
-    'Gave training courses and shared knowledge with the team, increasing the tech stack used across our projects.',
-    'Collaborated with other engineers to create a very complex system to calculate required equipment in the photovoltaic industry.'],
-    stack: ['Vue', 'NodeJs', 'Quasar', 'Angular', 'PHP', 'Laravel']
-},
-{
+      "Developed dockerized applications in NodeJS, Laravel, Vue, Python and Svelte.",
+      "Maintained a reliable platform with millions of dollars transacted monthly.",
+      "Gave training courses and shared knowledge with the team, increasing the tech stack used across our projects.",
+      "Collaborated with other engineers to create a very complex system to calculate required equipment in the photovoltaic industry.",
+    ],
+    stack: ["Vue", "NodeJs", "Quasar", "Angular", "PHP", "Laravel"],
+  },
+  {
     startDate: "2020",
     endDate: "2020",
     jobTitle: "Self-employed",
     description: [
-        'Built and shipped a platform to subscribe to a monthly healthy food basket delivery service, using Angular, Ruby on Rails, PostgreSQL, and stripe for payment on a Digital Ocean infrastructure.',
-        'Architected and implemented a system to register and hire academic text reviewers, using React, Ruby on Rails and PostgreSQL',
-        'Created a React Component to process X-Ray for a clinical diagnostics company.',
-        'Improved a item select page for a food delivery company.'
+      "Built and shipped a platform to subscribe to a monthly healthy food basket delivery service, using Angular, Ruby on Rails, PostgreSQL, and stripe for payment on a Digital Ocean infrastructure.",
+      "Architected and implemented a system to register and hire academic text reviewers, using React, Ruby on Rails and PostgreSQL",
+      "Created a React Component to process X-Ray for a clinical diagnostics company.",
+      "Improved a item select page for a food delivery company.",
     ],
-    stack: ['Ruby on Rails', 'React', 'Angular', 'PostgreSQL']
-},
-{
+    stack: ["Ruby on Rails", "React", "Angular", "PostgreSQL"],
+  },
+  {
     startDate: "2015",
     endDate: "2020",
     jobTitle: "Web Developer",
     company: "Almix Internet",
     description: [
-    'Developed the company website and internal systems on Angular, PHP and MySQL.',
-    'Using Agile methods migrated all printed tickets to digital, highly increasing productivity and reducing paper waste.',
-    'Created the advertisement design and marketing of the company using Adobe Illustrator and Photoshop.'],
-    stack: ['PHP', 'Angular', 'MySQL']
-}
-]
+      "Developed the company website and internal systems on Angular, PHP and MySQL.",
+      "Using Agile methods migrated all printed tickets to digital, highly increasing productivity and reducing paper waste.",
+      "Created the advertisement design and marketing of the company using Adobe Illustrator and Photoshop.",
+    ],
+    stack: ["PHP", "Angular", "MySQL"],
+  },
+];
